24f34370330a839f86b6d684ed7638f25a26b65b
Ralph Tang <r33tang@uwaterloo.ca>
2018-09-15 13:41:58 -0400

    Add sequential run

--- examples/measure_conv2d.py
+++ examples/measure_conv2d.py
@@ -1,5 +1,3 @@
-import gc
-
 from neurometer import LatencyWatch
 from tqdm import tqdm
 import fire
@@ -9,8 +7,8 @@ import torch.nn as nn
 
 class Conv2dBenchmark(object):
 
-    def run(self, features_in, features_out, height, width, step=5, kernel_size=3, padding=1, stride=1,
-            cuda=True, n_trials=300, burn_in=100, cache_clear_size=10000000):
+    def run(self, features_in, features_out, height, width, kernel_size=3, padding=1, stride=1,
+            cuda=True, n_trials=300, burn_in=100, cache_clear_size=10000000, main=True):
         ones = torch.ones(1, features_in, height, width)
         conv2d = nn.Conv2d(features_in, features_out, kernel_size, padding=padding, stride=stride)
         if cuda:
@@ -31,7 +29,14 @@ class Conv2dBenchmark(object):
             c = torch.zeros(cache_clear_size)
             if cuda:
                 c.cuda()
-        watch.write()
+        if main:
+            watch.write()
+        return watch.mean
+
+    def run_sequential(self, begin_features=1, end_features=1000, step=5):
+        for feat in tqdm(range(begin_features, end_features + 1, step), position=0):
+            measure = self.run(feat, 64, 64, 64, main=False)
+            print(f"{feat},{measure}")
 
 
 if __name__ == "__main__":
